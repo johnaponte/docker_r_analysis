774e43562c19aa837a3e73ef67f69dc3f4819673
John J. Aponte <john.j.aponte@gmail.com>
2025-05-09 15:20:36 +0200

    Change to allow name tag

--- build_image.sh
+++ build_image.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# Usage: ./build_image <tag>
-# Example: ./build_image 4.4.3
+# Usage: ./build_image <R_VERSION> <TAG>
+# Example: ./build_image 4.4.3 latest
 # by JJAV 2025
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <R_VERSION>"
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 <R_VERSION> <TAG>"
   exit 1
 fi
 
 R_VERSION="$1"
-IMAGE_NAME="r_analysis:${R_VERSION}"
+TAG="$2"
+IMAGE_NAME="r_analysis_${R_VERSION}:${TAG}"
 
 docker build \
   --build-arg R_VERSION="${R_VERSION}" \
@@ -17,4 +18,3 @@ docker build \
   .
 
 echo "Image ${IMAGE_NAME} built successfully."
-
